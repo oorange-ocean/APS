e4ae016f4a7123b83628ed222dd45fcdd048112d
Gary <garymur301@gmail.com>
2024-07-22 18:14:14 +0800

    feat: feishu login

--- src/api/login.js
+++ src/api/login.js
@@ -3,55 +3,63 @@ import axios from 'axios';
 import request from '@/utils/request'
 
 export async function login(username, password) {
-  const data = {
-    username,
-    password
-  }
-  return request({
-    url: '/system/index/login',
-    method: 'post',
-    data:data
-  })
+    const data = {
+        username,
+        password
+    }
+    return request({
+        url: '/system/index/login',
+        method: 'post',
+        data: data
+    })
 }
 
 // 注册方法
 export function register(data) {
-  return request({
-    url: '/register',
-    headers: {
-      isToken: false
-    },
-    method: 'post',
-    data: data
-  })
+    return request({
+        url: '/register',
+        headers: {
+            isToken: false
+        },
+        method: 'post',
+        data: data
+    })
 }
 
 // 获取用户详细信息
 export function getInfo() {
-  return request({
-    url: '/system/index/info',
-    method: 'get'
-  })
+    return request({
+        url: '/system/index/info',
+        method: 'get'
+    })
 }
 
 // 退出登录
 export function logout() {
-  return request({
-    url: '/system/index/exit',
-    method: 'post'
-  })
+    return request({
+        url: '/system/index/exit',
+        method: 'post'
+    })
 }
 
 //修改自己密码
-export function updatePwd(oldPassword,newPassword,reNewPassowrd) {
-  const data = {
-    oldPassword,
-    newPassword,
-    reNewPassowrd
-  }
-  return request({
-    url: '/system/sysUser/updatePwdByUser',
-    method:'post',
-    data:data
-  })
+export function updatePwd(oldPassword, newPassword, reNewPassowrd) {
+    const data = {
+        oldPassword,
+        newPassword,
+        reNewPassowrd
+    }
+    return request({
+        url: '/system/sysUser/updatePwdByUser',
+        method: 'post',
+        data: data
+    })
+}
+
+//飞书登录 get方法，参数为飞书预登录码，返回用户身份信息
+export function feishuLogin(code) {
+    return request({
+        url: '/system/index/codeLogin?code=' + code,
+        method: 'get'
+    })
 }
--- vite.config.js
+++ vite.config.js
@@ -7,24 +7,24 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    // https://cn.vitejs.dev/config/#resolve-alias
-    alias: {
-      // 设置路径
-      "~": path.resolve(__dirname, "./"),
-      // 设置别名
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ],
+    resolve: {
+        // https://cn.vitejs.dev/config/#resolve-alias
+        alias: {
+            // 设置路径
+            "~": path.resolve(__dirname, "./"),
+            // 设置别名
+            "@": path.resolve(__dirname, "./src"),
+        },
+        // https://cn.vitejs.dev/config/#resolve-extensions
+        extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
     },
-    // https://cn.vitejs.dev/config/#resolve-extensions
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
-  },
 })
